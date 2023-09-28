7decc30cdd4bae7c6a310b065b974db9d5e1b3f1
Eugene Hwang <38165503+eugeneh-wang@users.noreply.github.com>
2023-09-28 18:12:12 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 
 const dev = process.argv.includes('dev');
 
-// /** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
